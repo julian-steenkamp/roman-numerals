bd90b5c0d4f5d9a5124fd3f812a5f122e6c357ac
Julian Steenkamp <julian.steenkamp@kurtosys.com>
2024-10-16 08:25:12 +0200

    Update Program.cs

--- RomanNumerals/Program.cs
+++ RomanNumerals/Program.cs
@@ -1,23 +1,20 @@
-﻿namespace RomanNumerals
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine(RomanToInteger.Convert("III"));       // Output: 3
-            Console.WriteLine(RomanToInteger.Convert("IV"));        // Output: 4
-            Console.WriteLine(RomanToInteger.Convert("IX"));        // Output: 9
-            Console.WriteLine(RomanToInteger.Convert("LVIII"));     // Output: 58
-            Console.WriteLine(RomanToInteger.Convert("MCMXCIV"));   // Output: 1994
-            Console.WriteLine(RomanToInteger.Convert("MMMCMXCIX")); // Output: 3999
-        }
+        Console.WriteLine(RomanToInteger.Convert("III"));       // Output: 3
+        Console.WriteLine(RomanToInteger.Convert("IV"));        // Output: 4
+        Console.WriteLine(RomanToInteger.Convert("IX"));        // Output: 9
+        Console.WriteLine(RomanToInteger.Convert("LVIII"));     // Output: 58
+        Console.WriteLine(RomanToInteger.Convert("MCMXCIV"));   // Output: 1994
+        Console.WriteLine(RomanToInteger.Convert("MMMCMXCIX")); // Output: 3999
     }
+}
 
-    public class RomanToInteger
+public class RomanToInteger
+{
+    public static int Convert(string value)
     {
-        public static int Convert(string value)
-        {
-             //Todo: Convert Roman Numerals to Integer.
-        }
+         //Todo: Convert Roman Numerals to Integer.
     }
 }
