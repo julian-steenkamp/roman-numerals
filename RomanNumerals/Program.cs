34f51cfabae81f60aaef558589732385d0b751af
Julian Steenkamp <julian.steenkamp@kurtosys.com>
2024-10-16 08:20:41 +0200

    Update Program.cs

--- RomanNumerals/Program.cs
+++ RomanNumerals/Program.cs
@@ -2,9 +2,22 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //Todo: Convert Roman Numerals to Numbers.
+            Console.WriteLine(RomanToInteger.Convert("III"));       // Output: 3
+            Console.WriteLine(RomanToInteger.Convert("IV"));        // Output: 4
+            Console.WriteLine(RomanToInteger.Convert("IX"));        // Output: 9
+            Console.WriteLine(RomanToInteger.Convert("LVIII"));     // Output: 58
+            Console.WriteLine(RomanToInteger.Convert("MCMXCIV"));   // Output: 1994
+            Console.WriteLine(RomanToInteger.Convert("MMMCMXCIX")); // Output: 3999
+        }
+    }
+
+    public class RomanToInteger
+    {
+        public static int Convert(string value)
+        {
+             //Todo: Convert Roman Numerals to Integer.
         }
     }
 }
